0bd365f29e5c6ebf70be1980b73803a7718d6aea
IndraK <81152353+frex-e@users.noreply.github.com>
2024-06-09 01:03:58 +1000

    final fix

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
         functions</MyLink><div class="text-orange-500 border-orange-500"></div>
       to use in
       VCE Maths exams. (Totally not cheating)
-      <span class="text-blue-500"></span>
+      <span class="text-blue-500 border-orange-500"></span>
 
       <NuxtLink to="/blog" class="block mb-4 mt-8 sm:mt-16 text-xl sm:text-xl text-red-600 border-red-600 border-2 w-min 
       whitespace-nowrap p-2 rounded-lg hover:bg-red-600 hover:text-white transition-colors"> Check Out My Blog</NuxtLink>
